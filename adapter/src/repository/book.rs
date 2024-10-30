64202e967c96f72a3bec24a6e30bf0f4d2f0c67f
yuukimasir0 <plusion00@icloud.com>
2024-10-30 13:31:58 +0900

    adapter test

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -217,6 +217,8 @@ impl BookRepositoryImpl {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
     use crate::repository::user::UserRepositoryImpl;
     use kernel::{model::user::event::CreateUser, repository::user::UserRepository};
@@ -270,6 +272,23 @@ mod tests {
         assert_eq!(isbn, "Test ISBN");
         assert_eq!(description, "Test Description");
         assert_eq!(owner.name, "Test User");
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures("common", "book"))]
+    async fn test_update_book(pool: sqlx::PgPool) -> anyhow::Result<()> {
+        let repo = BookRepositoryImpl::new(ConnectionPool::new(pool.clone()));
+
+        let book_id = BookId::from_str("9890736e-14e4-461a-a77d-eac3517ef11b").unwrap();
+        let book = repo.find_by_id(book_id).await?.unwrap();
+        const NEW_AUTHOR: &str = "更新後の著者名";
+        assert_ne!(book.author, NEW_AUTHOR);
+
+        let update_book = UpdateBook { book_id, title: book.title, author: NEW_AUTHOR.into(), isbn: book.isbn, description: book.description, requested_user: UserId::from_str("5b4c96ac-316a-4bee-8e69-cac5eb84ff4c").unwrap() };
+        repo.update(update_book).await.unwrap();
+
+        let book = repo.find_by_id(book_id).await?.unwrap();
+        assert_eq!(book.author, NEW_AUTHOR);
         Ok(())
     }
 }
